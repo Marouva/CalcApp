38ca50ca65bd9fcaa451e190a87f217bc3396896
Václav Maroušek <vaclav.marousek@gmail.com>
2020-10-11 17:22:49 +0200

    Funkční build

--- CalcApp/CalcApp/MainPage.xaml.cs
+++ CalcApp/CalcApp/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,46 +19,91 @@ namespace CalcApp
 
 		public string buffer = "";
 
-		public void Update()
+		public void AddChar(char ch)
 		{
-			result.Text = buffer;
+			buffer += ch;
+
+			UpdateDisplay(buffer);
 		}
 
-		public void Equals()
-        {
+		public void RemoveLastChar()
+		{
+			if (buffer.Length != 0)
+				buffer = buffer.Substring(0, buffer.Length - 1);
 
+			UpdateDisplay(buffer);
+		}
 
-			result.Text = ((int)0).ToString();
-        }
+		public void ClearBuffer()
+		{
+			buffer = "";
+
+			UpdateDisplay(buffer);
+		}
+		
+		public void EvalBuffer()
+		{
+			// Error checking
+			if (String.IsNullOrWhiteSpace(buffer))
+			{
+				result.Text = "";
+				return;
+			}
+
+			// Evaluation
+			try
+			{
+				DataTable dataTable = new DataTable();
+				decimal resultValue = Convert.ToDecimal(dataTable.Compute(buffer, ""));
+
+				buffer = resultValue.ToString();
+				UpdateDisplay(buffer);
+			}
+			catch (Exception e)
+			{
+				buffer = "";
+				UpdateDisplay(e.Message);
+			}
+		}
+
+		public void UpdateDisplay(string text)
+		{
+			result.Text = text.Replace('+', '+')
+							  .Replace('-', '−')
+							  .Replace('*', '×')
+							  .Replace('/', '÷')
+							  .Replace('^', '^')
+							  .Replace('√', '√');
+		}
 
 		#region handlers
 
 		/* Numbers */
-		private void keypad0_Clicked(object sender, EventArgs e) { buffer += "0"; Update(); }
-		private void keypad1_Clicked(object sender, EventArgs e) { buffer += "1"; Update(); }
-		private void keypad2_Clicked(object sender, EventArgs e) { buffer += "2"; Update(); }
-		private void keypad3_Clicked(object sender, EventArgs e) { buffer += "3"; Update(); }
-		private void keypad4_Clicked(object sender, EventArgs e) { buffer += "4"; Update(); }
-		private void keypad5_Clicked(object sender, EventArgs e) { buffer += "5"; Update(); }
-		private void keypad6_Clicked(object sender, EventArgs e) { buffer += "6"; Update(); }
-		private void keypad7_Clicked(object sender, EventArgs e) { buffer += "7"; Update(); }
-		private void keypad8_Clicked(object sender, EventArgs e) { buffer += "8"; Update(); }
-		private void keypad9_Clicked(object sender, EventArgs e) { buffer += "9"; Update(); }
+		private void keypad0_Clicked(object sender, EventArgs e) { AddChar('0'); }
+		private void keypad1_Clicked(object sender, EventArgs e) { AddChar('1'); }
+		private void keypad2_Clicked(object sender, EventArgs e) { AddChar('2'); }
+		private void keypad3_Clicked(object sender, EventArgs e) { AddChar('3'); }
+		private void keypad4_Clicked(object sender, EventArgs e) { AddChar('4'); }
+		private void keypad5_Clicked(object sender, EventArgs e) { AddChar('5'); }
+		private void keypad6_Clicked(object sender, EventArgs e) { AddChar('6'); }
+		private void keypad7_Clicked(object sender, EventArgs e) { AddChar('7'); }
+		private void keypad8_Clicked(object sender, EventArgs e) { AddChar('8'); }
+		private void keypad9_Clicked(object sender, EventArgs e) { AddChar('9'); }
 
-		private void keypadDecimal_Clicked(object sender, EventArgs e) { buffer += "."; Update(); }
+		private void keypadDecimal_Clicked(object sender, EventArgs e) { AddChar('.'); }
 
 		/* Operations */
-		private void keypadAddition_Clicked(object sender, EventArgs e)		  { buffer += "+"; Update(); }
-		private void keypadSubtraction_Clicked(object sender, EventArgs e)	  { buffer += "−"; Update(); }
-		private void keypadMultiplication_Clicked(object sender, EventArgs e) { buffer += "×"; Update(); }
-		private void keypadDivision_Clicked(object sender, EventArgs e)		  { buffer += "÷"; Update(); }
-		private void keypadPower_Clicked(object sender, EventArgs e)		  { buffer += "^"; Update(); }
-		private void keypadRoot_Clicked(object sender, EventArgs e)			  { buffer += "√"; Update(); }
+		private void keypadAddition_Clicked(object sender, EventArgs e)		  { AddChar('+'); }
+		private void keypadSubtraction_Clicked(object sender, EventArgs e)	  { AddChar('-'); }
+		private void keypadMultiplication_Clicked(object sender, EventArgs e) { AddChar('*'); }
+		private void keypadDivision_Clicked(object sender, EventArgs e)		  { AddChar('/'); }
+		private void keypadPower_Clicked(object sender, EventArgs e)		  { AddChar('^'); }
+		private void keypadRoot_Clicked(object sender, EventArgs e)			  { AddChar('√'); }
 
-        /* Actions */
-        private void keypadClear_Clicked(object sender, EventArgs e)  { buffer = ""; Update(); }
-        private void keypadErase_Clicked(object sender, EventArgs e)  { if (buffer.Length != 0) { buffer = buffer.Substring(0, buffer.Length - 1); Update(); } }
-        private void keypadEquals_Clicked(object sender, EventArgs e) { Equals(); }
+		/* Actions */
+		private void keypadClear_Clicked(object sender, EventArgs e)  { ClearBuffer(); }
+		private void keypadErase_Clicked(object sender, EventArgs e)  { RemoveLastChar(); }
+		private void keypadEquals_Clicked(object sender, EventArgs e) { EvalBuffer(); }
 
 		#endregion
 	}
